0131c483dc81a3e69a27d60791aabfc5c5af4337
yudiatmoko <yudiatmokoilham@gmail.com>
2023-09-30 00:35:03 +0700

    Finishing App

--- app/src/main/java/com/catnip/egroceries/presentation/main/MainActivity.kt
+++ app/src/main/java/com/catnip/egroceries/presentation/main/MainActivity.kt
@@ -3,22 +3,42 @@ package com.catnip.egroceries.presentation.main
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.catnip.egroceries.R
+import com.catnip.egroceries.data.datasource.local.datastore.UserPreferenceDataSourceImpl
+import com.catnip.egroceries.data.datasource.local.datastore.appDataStore
 import com.catnip.egroceries.databinding.ActivityMainBinding
+import com.catnip.egroceries.utils.GenericViewModelFactory
+import com.catnip.egroceries.utils.PreferenceDataStoreHelperImpl
 
 class MainActivity : AppCompatActivity() {
     private val binding: ActivityMainBinding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
-    private val viewModel: MainViewModel by viewModels()
+
+    private val viewModel: MainViewModel by viewModels {
+        val dataStore = this.appDataStore
+        val dataStoreHelper = PreferenceDataStoreHelperImpl(dataStore)
+        val userPreferenceDataSource = UserPreferenceDataSourceImpl(dataStoreHelper)
+        GenericViewModelFactory.create(MainViewModel(userPreferenceDataSource))
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
         setupBottomNav()
         //todo : observe value dark mode for first launch
+        observeDarkModePref()
+    }
+
+    private fun observeDarkModePref() {
+        viewModel.userDarkModeLiveData.observe(this){
+            AppCompatDelegate.setDefaultNightMode(
+                if (it) AppCompatDelegate.MODE_NIGHT_YES else AppCompatDelegate.MODE_NIGHT_NO
+            )
+        }
     }
 
     private fun setupBottomNav() {
--- app/src/main/java/com/catnip/egroceries/presentation/settings/SettingsDialogFragment.kt
+++ app/src/main/java/com/catnip/egroceries/presentation/settings/SettingsDialogFragment.kt
@@ -4,17 +4,29 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
+import com.catnip.egroceries.data.datasource.local.datastore.UserPreferenceDataSourceImpl
+import com.catnip.egroceries.data.datasource.local.datastore.appDataStore
 import com.catnip.egroceries.databinding.FragmentSettingsDialogBinding
+import com.catnip.egroceries.presentation.main.MainViewModel
+import com.catnip.egroceries.utils.GenericViewModelFactory
+import com.catnip.egroceries.utils.PreferenceDataStoreHelperImpl
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
 
 class SettingsDialogFragment : BottomSheetDialogFragment() {
 
-
     private lateinit var binding: FragmentSettingsDialogBinding
 
-    private val viewModel: SettingsViewModel by viewModels()
+    private val mainViewModel : MainViewModel by activityViewModels()
+
+    private val viewModel: SettingsViewModel by viewModels{
+        val dataStore =  this.requireContext().appDataStore
+        val dataStoreHelper = PreferenceDataStoreHelperImpl(dataStore)
+        val userPreferenceDataSource = UserPreferenceDataSourceImpl(dataStoreHelper)
+        GenericViewModelFactory.create(SettingsViewModel(userPreferenceDataSource))
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -28,12 +40,20 @@ class SettingsDialogFragment : BottomSheetDialogFragment() {
         super.onViewCreated(view, savedInstanceState)
         setSwitchAction()
         //todo : observe value dark mode
+        setSwitchAction()
+        observeDarkModePref()
+    }
+
+    private fun observeDarkModePref() {
+        mainViewModel.userDarkModeLiveData.observe(this){
+            binding.swDarkMode.isChecked = it
+        }
     }
 
 
     private fun setSwitchAction() {
         binding.swDarkMode.setOnCheckedChangeListener { _, isUsingDarkMode ->
-            //todo : set value dark mode to viewmodel
+            viewModel.setDarkModePref(isUsingDarkMode)
         }
     }
 }
